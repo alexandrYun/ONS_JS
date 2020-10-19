193f2daf03f3c99c74cb6c2021a641ddbba671e0
alexandrYun <alexandryun@mail.ru>
2020-10-19 23:03:45 +0300

    Game ready

---
+++ DEK Game/script.js
@@ -0,0 +1,81 @@
+class Enemy {
+    constructor(name, hp, color, damage, range) {
+        this.name = name;
+        this.hp = hp;
+        this.color = color;
+        this.damage = damage;
+        this.range = range;
+    }
+    Attack(target, range) {
+        targets[target].hp -= this.damage;
+        console.log(range);
+    }
+    getDamage(damage) {
+        this.hp -= damage;
+    }
+    scream() {
+        console.log(this.name);
+    }
+}
+
+let targets = [
+    {name: 'target1', hp: 50, targetRange: 20, targetDamage: 5},
+    {name: 'target2', hp: 50, targetRange: 30, targetDamage: 5},
+    {name: 'target3', hp: 50, targetRange: 10, targetDamage: 5},
+    {name: 'target4', hp: 50, targetRange: 20, targetDamage: 5},
+    {name: 'target5', hp: 50, targetRange: 30, targetDamage: 5},
+    {name: 'target6', hp: 50, targetRange: 30, targetDamage: 5},
+    {name: 'target7', hp: 50, targetRange: 20, targetDamage: 5}
+]
+
+class Knight extends Enemy {
+    constructor(name) {
+        super(name, 50, 'red', 10, 20);
+    }
+    AttackAll() {
+        for(let knightTar of targets) {
+            if(knightTar.targetRange <= 20) {
+                knightTar.hp -= 30;
+                console.log(`${knightTar.name}, к рыцарю лучше не подходить!!!`);
+            } else {
+                console.log(`${knightTar.name} cлишком далеко`);
+            }
+        }
+    }
+}
+
+class Elf extends Enemy {
+    constructor(name) {
+        super(name, 50, 'blue', 8, 30);
+    }
+    MagicAttack(elfTarget) {
+        if(targets[elfTarget].targetRange <= 20) {
+            targets[elfTarget].hp -= 50;
+            console.log('Авада Кедавра!!!');
+            console.log(`${targets[elfTarget].name} убит!!!`);
+        } else {
+            console.log(`${targets[elfTarget].name} слишком далеко`);
+        }
+    }
+}
+
+class Dwarf extends Enemy {
+    constructor(name) {
+        super(name, 50, 'grey', 12, 10);
+    }
+    ThrowAxe(dwarfTarget) {
+        if(targets[dwarfTarget].targetRange <= 30) {
+            targets[dwarfTarget].hp -= 50;
+            console.log('Звучит свист топоров!!!');
+            console.log(`${targets[dwarfTarget].name} убит!!!`);
+        } else {
+            console.log(`${targets[dwarfTarget].name} слишком далеко`);
+        }
+    }
+}
+
+let knight = new Knight('knight');
+
+let elf = new Elf('elf');
+
+let dwarf = new Dwarf('dwarf');
